c531b1e37827fd942bfd02e81207fb8d9e06c768
mmast <mmast@mmt.local>
2018-06-07 11:12:16 +0200

    Well done

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	router := gin.Default()
+	router.GET("/ajaxify", func(c *gin.Context) {
+		c.JSON(404, gin.H{"msg": "this feature is not implemented"})
+	})
+	router.Run()
+}
